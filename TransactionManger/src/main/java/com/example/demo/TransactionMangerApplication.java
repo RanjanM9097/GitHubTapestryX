6c250cfbab25b440eff080e99905ca080dffcff2
ranjan@appensure.com <root@gitlab.testing>
2021-07-20 13:17:26 +0530

    TranactionManger main branch

---
+++ TransactionManger/src/main/java/com/example/demo/TransactionMangerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionMangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionMangerApplication.class, args);
+	}
+
+}
